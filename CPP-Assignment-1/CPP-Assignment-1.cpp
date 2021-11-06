6a67fb237afd8a2f674cb89325c60c4726568698
s3681437 <s3681437@rmit.edu.vn>
2021-11-06 14:03:34 +0700

    Add calculate covariance function
    Add calculate covariance function

--- CPP-Assignment-1/CPP-Assignment-1.cpp
+++ CPP-Assignment-1/CPP-Assignment-1.cpp
@@ -53,6 +53,26 @@ void print_array(int arr[], int size) {
 	cout << endl;
 }
 
+// A function to calculate mean  
+float mean(float arr[], int n) // n is number of elements in the set
+{
+	float sum = 0;
+	for (int i = 0; i < n; i++)
+		sum = sum + arr[i];
+	return sum / n;
+}
+
+// A function to calculate covariance of 2 set 
+float covariance(float arr1[], float arr2[], int n) // n is number of elements in each of the sets, they have to be equal to find the covariance
+{
+	float sum = 0;
+	for (int i = 0; i < n; i++)
+		sum = sum + (arr1[i] - mean(arr1, n)) * (arr2[i] - mean(arr2, n));
+	return sum / (n - 1);
+}
+
+
+
 // Driver program to test above functions
 int main(int argc, char* argv[])
 {
@@ -74,5 +94,20 @@ int main(int argc, char* argv[])
 	print_array(arr, n);
 
 	delete[] arr;
+
+//Hoang's part: Calculate Covairance
+
+	// Example set of data
+	float arr1[] = { 2.1, 3.2, 15.6, 9.9, 20.4};
+	int n1 = sizeof(arr1) / sizeof(arr1[0]);	// To find the amount of element in a array 1 
+
+	float arr2[] = { 4.9, 6.0, 23.5, 7.7, 15.15};
+	int n2 = sizeof(arr2) / sizeof(arr2[0]);	// To find the amount of element in a array 2
+
+	if (n1 == n2)
+		cout <<"cov(x_y)= " << covariance(arr1, arr2, n1) << endl;
+	else
+		cout << "Can not find covariance because there needs to be the same amount of elements in each set." << endl;
+
 	return 0;
 }

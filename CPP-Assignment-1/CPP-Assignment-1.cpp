b35d2720bda3e1e688a1743e022bb27e164f4a3f
s3681437 <s3681437@rmit.edu.vn>
2021-11-13 19:31:59 +0700

    Add function to cal correlation coefficient

--- CPP-Assignment-1/CPP-Assignment-1.cpp
+++ CPP-Assignment-1/CPP-Assignment-1.cpp
@@ -54,25 +54,50 @@ void print_array(int arr[], int size) {
 }
 
 // A function to calculate mean  
-float mean(float arr[], int n) // n is number of elements in the set
+float cal_mean(float arr[], int n) // n is number of elements in the set
 {
-	float sum = 0;
+	float sumOfArr = 0;
 	for (int i = 0; i < n; i++)
-		sum = sum + arr[i];
-	return sum / n;
+		sumOfArr = sumOfArr + arr[i];
+	return sumOfArr / n;
 }
 
-// A function to calculate covariance of 2 set 
-float covariance(float arr1[], float arr2[], int n) // n is number of elements in each of the sets, they have to be equal to find the covariance
+// A function to calculate covariance of 2 sets 
+float cal_covariance(float arr1[], float arr2[], int n) // n is number of elements in each of the sets
 {
 	float sum = 0;
 	for (int i = 0; i < n; i++)
-		sum = sum + (arr1[i] - mean(arr1, n)) * (arr2[i] - mean(arr2, n));
+		sum = sum + (arr1[i] - cal_mean(arr1, n)) * (arr2[i] - cal_mean(arr2, n)); //This is the formula to calculate covariance
 	return sum / (n - 1);
 }
 
+// A function to calculate correlationCoefficient of 2 set
+float cal_correlationCoefficient(float arr1[], float arr2[], int n)
+{
 
+	float sum_arr1 = 0, sum_arr2 = 0, sumOfArr1XArr2 = 0;
+	float squareSum_arr1 = 0, squareSum_arr2 = 0;
 
+	for (int i = 0; i < n; i++)
+	{
+		//The sum of elements of 1st and 2nd set .
+		sum_arr1 = sum_arr1 + arr1[i];
+		sum_arr2 = sum_arr2 + arr2[i];
+
+		// sum of elements of 1st array * elements of 2nd set.
+		sumOfArr1XArr2 = sumOfArr1XArr2 + arr1[i] * arr2[i];
+
+		// sum of square of each elements in each sets.
+		squareSum_arr1 = squareSum_arr1 + arr1[i] * arr1[i];
+		squareSum_arr2 = squareSum_arr2 + arr2[i] * arr2[i];
+	}
+
+	// Calculate correlation coefficient using this formula:
+	float co_co =(n * sumOfArr1XArr2 - sum_arr1 * sum_arr2)
+				/ sqrt((n * squareSum_arr1 - sum_arr1 * sum_arr1) * (n * squareSum_arr2 - sum_arr2 * sum_arr2));
+
+	return co_co;
+}
 // Driver program to test above functions
 int main(int argc, char* argv[])
 {
@@ -95,19 +120,30 @@ int main(int argc, char* argv[])
 
 	delete[] arr;
 
-//Hoang's part: Calculate Covairance
+//Hoang's part: Calculate Covairance, Correlation coefficient
 
 	// Example set of data
-	float arr1[] = { 2.1, 3.2, 15.6, 9.9, 20.4};
+	float arr1[] = {1.5,2.8,23.6,14.5};
 	int n1 = sizeof(arr1) / sizeof(arr1[0]);	// To find the amount of element in a array 1 
 
-	float arr2[] = { 4.9, 6.0, 23.5, 7.7, 15.15};
+	float arr2[] = {5.6,7.7,8.1,9.3};
 	int n2 = sizeof(arr2) / sizeof(arr2[0]);	// To find the amount of element in a array 2
 
-	if (n1 == n2)
-		cout <<"cov(x_y)= " << covariance(arr1, arr2, n1) << endl;
-	else
+	//Print out the Covariance
+	if (n1 == n2){
+		cout << "cov(x_y)= " << cal_covariance(arr1, arr2, n1) << endl;
+	}
+	else {
 		cout << "Can not find covariance because there needs to be the same amount of elements in each set." << endl;
+	}
 
+	//Print out the Correlation Coefficient
+	if (n1 == n2) {
+		cout << "r(x_y)= " << cal_correlationCoefficient(arr1, arr2, n1) << endl;
+	}
+	else {
+		cout << "Can not find covariance because there needs to be the same amount of elements in each set." << endl;
+	}
+	
 	return 0;
 }

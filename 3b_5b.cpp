be5da6cc867cf6d26b76d9f3016449ce0b093605
kakenpi <84964337+kakenpi@users.noreply.github.com>
2021-11-24 17:15:32 +0700

    Update 3b_5b.cpp

--- 3b_5b.cpp
+++ 3b_5b.cpp
@@ -6,43 +6,23 @@
 
 using namespace std;
 
-// Compute Variance and Standard Deviation of array x
-float varianceX(float arr_x[], int& rows) {
+// Compute Variance and Standard Deviation
+float variance(float arr[], int& rows) {
     // Compute mean 
     float sum = 0;
     for (int i = 0; i < rows; i++)
-        sum += arr_x[i];
+        sum += arr[i];
     float mean = sum / rows;
 
     // Compute sum squared differences with mean.
     float sqDiff = 0;
     for (int i = 0; i < rows; i++)
-        sqDiff += (arr_x[i] - mean) * (arr_x[i] - mean);
+        sqDiff += (arr[i] - mean) * (arr[i] - mean);
     return sqDiff / rows;
 }
 
-float StandardDeviationX(float arr_x[], int rows) {
-    return sqrt(varianceX(arr_x, rows));
-}
-
-// Compute Variance and Standard Deviation of array y
-float varianceY(float arr_y[], int& rows) {
-    // Compute mean 
-    float sum = 0;
-    for (int i = 0; i < rows; i++)
-        sum += arr_y[i];
-    float mean = sum / rows;
-
-    // Compute sum squared differences with mean.
-    float sqDiff = 0;
-    for (int i = 0; i < rows; i++)
-        sqDiff += (arr_y[i] - mean) * (arr_y[i] - mean);
-    return sqDiff / rows;
-}
-
-
-float StandardDeviationY(float arr_y[], int rows) {
-    return sqrt(varianceY(arr_y, rows));
+float StandardDeviation(float arr[], int rows) {
+    return sqrt(variance(arr, rows));
 }
 
 // Compute Third Quartile 
@@ -63,10 +43,10 @@ int main() {
     float arr_x[] = { 8, 91, 63, 72, 40, 55 };
     float arr_y[] = { 7, 15, 36, 39, 40, 41 };
     int rows = sizeof(arr_x) / sizeof(arr_x[0]);
-    cout << "Var_x= " << varianceX(arr_x, rows);
-    cout << " - " << "Var_y= " << varianceY(arr_y, rows) << "\n";
-    cout << "stdev_x=  " << StandardDeviationX(arr_x, rows);
-    cout << " - " << "stdev_y = " << StandardDeviationY(arr_y, rows) << "\n";
+    cout << "Var_x= " << variance(arr_x, rows);
+    cout << " - " << "Var_y= " << variance(arr_y, rows) << "\n";
+    cout << "stdev_x=  " << StandardDeviation(arr_x, rows);
+    cout << " - " << "stdev_y = " << StandardDeviation(arr_y, rows) << "\n";
     ThirdQuartile(arr_x, arr_y, rows);
     return 0;
 }
